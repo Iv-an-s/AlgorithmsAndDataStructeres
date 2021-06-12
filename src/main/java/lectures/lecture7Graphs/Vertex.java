b610d96d296f613dad7d9bea83b25ff791e39f84
Ivan <ivan_semenov@bk.ru>
2021-06-12 11:48:37 +0300

    add lecture7 classes

---
+++ src/main/java/lectures/lecture7Graphs/Vertex.java
@@ -0,0 +1,45 @@
+package lectures.lecture7Graphs;
+
+import java.util.Objects;
+
+public class Vertex {
+
+    private final String label; // Значение вершиныю Обозначение того, что она из себя представляет.
+    private boolean visited;
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vertex)) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(getLabel(), vertex.getLabel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLabel());
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "label='" + label + '\'' +
+                '}';
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+}
